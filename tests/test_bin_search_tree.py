90b6220460af61b20de39aa571559dceb43042b8
pponn <156141576+pponn@users.noreply.github.com>
2025-01-31 12:39:44 -0500

    Update test_bin_search_tree.py

--- tests/test_bin_search_tree.py
+++ tests/test_bin_search_tree.py
@@ -1,3 +1,9 @@
+#numbers should be bigger than this:
+#both trees 260,000 unique keys (nodes)
+#AVL tree height 21
+#AVL tree built in 178 sec
+#average link of lists of documents for which word appears = 268
+
 """
 This module contains unit tests for the BinarySearchTreeIndex class.
 
@@ -11,13 +17,56 @@ The following tests are included:
 - `test_get_keys_in_order`: Tests the `get_keys_in_order` method of the BinarySearchTreeIndex class.
 - `test_get_leaf_keys`: Tests the `get_leaf_keys` method of the BinarySearchTreeIndex class.
 """
-
-
 import pytest
 from indexer.trees.bst_index import BinarySearchTreeIndex
 from indexer.trees.bst_node import BSTNode
+from indexer.trees.avl_node import AVLNode
+from indexer.trees.avl_tree import AVLTreeIndex
+
+#AVL tests:
+@pytest.fixture
+def avl():
+  return AVLTreeIndex()
+
+def test_insert_and_search_avl(avl):
+  avl.insert('d', 4)
+  avl.insert('e', 5)
+  avl.insert('f', 6) 
+
+  assert avl.search('d') == [4]
+  assert avl.search('e') == [5]
+  assert avl.search('f') == [6]
+
+def test_count_nodes_avl(avl):
+  avl.insert('d', 4)
+  avl.insert('e', 5)
+  avl.insert('f', 6)
+  
+  assert avl.count_nodes() == 3
+
+def test_tree_height_avl(avl):
+  avl.insert('d', 4)
+  avl.insert('e', 5)
+  avl.insert('f', 6)
+  
+  assert avl.tree_height() == 2
+
+def test_get_keys_in_order_avl(avl):
+  avl.insert('d', 4)
+  avl.insert('e', 5)
+  avl.insert('f', 6)
+
+  assert avl.get_keys_in_order() == ['d', 'e', 'f']
+
+def test_get_leaf_keys_avl(avl):
+  avl.insert('d', 4)
+  avl.insert('e', 5)
+  avl.insert('f', 6)
+  
+  assert avl.get_leaf_keys() == ['d', 'f']
 
 
+#BST tests:
 @pytest.fixture
 def bst():
   return BinarySearchTreeIndex()

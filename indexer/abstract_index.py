989599d1fa9f961469f6b44eb90e86a06a613c48
pponn <156141576+pponn@users.noreply.github.com>
2025-01-31 12:38:27 -0500

    Update abstract_index.py

--- indexer/abstract_index.py
+++ indexer/abstract_index.py
@@ -1,22 +1,26 @@
 from typing import List, Optional, Any, Generator
 from abc import ABC, abstractmethod
 
-# from indexer.trees.bst_node import BSTNode
+from indexer.trees.bst_node import BSTNode
 
 
 class AbstractIndex(ABC):
     def __init__(self):
-        #self.root: Optional[BSTNode] = None
-        pass
+       self.values: List[Any] = []
+       self.left: Optional['BSTNode'] = None
+       self.right: Optional['BSTNode'] = None
+
 
     @abstractmethod
     def insert(self, key: Any, value: Any) -> None:
-        pass
+        self.root = self._insert_recursive(self.root, key, value)
+
 
     @abstractmethod
     def search(self, key: Any) -> List[Any]:
-        pass
+        return self._search_recursive(self.root, key)
 
     @abstractmethod
     def __iter__(self) -> Generator[Any, None, None]:
-        pass
+        yield from self._inorder_traversal_generator(self.root)
+
